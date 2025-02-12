a3b16cb30702cfa72cd8b1b569843d83803eec19
Brandon Jones <brandonjones435@u.boisestate.edu>
2025-02-12 16:58:14 -0700

    Working with new stuff

--- DriverClass.java
+++ DriverClass.java
@@ -1,10 +1,14 @@
 public class DriverClass {
     // This is a drivers class to help me see what the methods are doing
-    private static int[] array = {0,1,2,3,4,5};
+    private static int[] array = {7,4,9,3,4,5};
     private static int value = 5; 
     private static int number;
+    private static int oldValue = 5;
+    private static int newValue = 6; 
+
     public static void main(String[] args) {
         number = MethodsToAnalyze.find(array, value);
         System.out.print(number);
+        MethodsToAnalyze.replaceAll(array, oldValue, newValue);
     }
 }
